90977330d3950424c0a8f91f2347e1fbf6d7a347
cesaraugusto98 <cesaraugusto.santos@outlook.com>
2018-08-06 20:13:29 -0300

    Configuração inicial -GitClass sample

---
+++ src/main/java/edu/fatec/gitclass/GitclassApplication.java
@@ -0,0 +1,12 @@
+package edu.fatec.gitclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitclassApplication.class, args);
+	}
+}
